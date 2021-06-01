fa272dbce39f47bb1f72fc388479ff1dfbea8090
Justin Rush <reachjustin212@gmail.com>
2021-06-01 10:28:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.clickMe').click(function () {
+        $(this).hide(200);
+    });
+});
+$(document).click(function () {
+    $("input").click(function () {
+        alert("hello");
+    })
+})
